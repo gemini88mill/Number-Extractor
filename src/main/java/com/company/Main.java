26395c02fcede19abeac477031bac6516e13364a
Raphael <gemini88mill@gmail.com>
2015-12-17 00:57:53 -0500

    added new url reader

---
+++ src/main/java/com/company/Main.java
@@ -0,0 +1,48 @@
+package com.company;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class Main {
+
+    private String userName = "rmiller";
+    private String password = "14851485";
+    private String url = "http://www.insidecellularworld.com/SalesPerfG.php";
+
+    public static void main(String[] args) {
+	// write your code here
+
+        collectCellWorldData();
+    }
+
+    /**
+     * collectCellWorldData() -
+     *      collects the data from insidecellularworld.com and formats into desired output.
+     */
+    private static void collectCellWorldData() {
+        Main main = new Main();
+
+        main.openCellularWorld();
+    }
+
+    private void openCellularWorld() {
+        //todo open webpage
+        try {
+            URL insideCellularWorld = new URL(url);
+            BufferedReader in = new BufferedReader(new InputStreamReader(insideCellularWorld.openStream()));
+
+            String inputLine;
+            while ((inputLine = in.readLine()) != null){
+                System.out.println(inputLine);
+            }
+            in.close();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
